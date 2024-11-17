6de56616021f899a503d6e660af46b52de6a3d41
chandusreeram-9399 <sekharchandra9399@gmail.com>
2024-11-17 23:35:53 +0530

    assistant has been added

---
+++ assistant.py
@@ -0,0 +1,63 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener=sr.Recognizer()
+engine=pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty("voice", voices[1].id)
+
+def talk(text):
+    
+    engine.say(text)
+    engine.runAndWait()
+ 
+def take_command():
+    try:
+       with sr.Microphone() as source:
+            print('listening....')
+            voice =listener.listen(source)
+            command=listener.recognize_google(voice)
+            command=command.lower()
+            if 'alexa' in command:
+                 command=command.replace("alexa",'')
+                 print(command)
+
+    except:
+          pass
+    return command    
+
+def run_alexa():
+    command=take_command()
+    print(command)
+    if 'play' in command:
+        song=command.replace('play','')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+          
+          time=datetime.datetime.now().strftime('%I:%M:%P')
+          print(time)
+          talk('current time is'+time)
+    elif 'wikipedia' in command:
+        person=command.replace('wikipedia','') 
+        info=wikipedia.summary(person,5) 
+        print(info)  
+        talk(info)
+    elif 'date' in command:
+        talk("sorry,i have an headache")
+    elif 'are you single' in  command:
+        talk("im in relationship with chandu") 
+   
+    elif 'cry'  in command:    
+    
+        talk("please say the command again")       
+while True:
+    run_alexa()
+
+
+
+
